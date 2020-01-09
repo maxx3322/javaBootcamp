537c1dcab1b8adf9ee408b5240e05e134aaf7322
maxx3322 <maxdgeers@gmail.com>
2020-01-09 15:06:47 +0800

    finsished
    finished project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Max";
+const favoriteNumber = Math.floor(Math.random() * 10);
+const secondNumber = Math.floor(Math.random() * 10);
+const answer = favoriteNumber * secondNumber;
+var hahaha = function() {
+  if (answer < 50) {
+    return "your are less than 50";
+  } else {
+    return "you are bigger than 50";
+  }
+};
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}</h1>
+    <p>My favorite number is {answer}</p>
+    <h2>{hahaha}</h2>
+  </div>,
+  document.getElementById("root")
+);
